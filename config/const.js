91a96e1ffbd6f7f6a4a9da4eaf70a024bc6cd916
chinavane <chinavane_2008@163.com>
2017-03-02 20:25:19 +0800

    添加了config目录
    config中加入了const常量内容，主要是包含提示信息，加密secret等内容。

---
+++ config/const.js
@@ -0,0 +1,9 @@
+exports.message = {
+  "error400":{"status":400,"msg":"系统操作异常，请重新确认",type:"warning"},
+  "error401":{"status":401,"msg":"用户操作未授权，请重新登录",type:"warning"},
+  "error402":{"status":402,"msg":"参数传递有误，请重新确认",type:"warning"},
+  "error403":{"status":403,"msg":"用户操作受限，请重新确认",type:"warning"},
+  "error404":{"status":404,"msg":"查询记录未找到，请重新确认",type:"warning"},
+};
+
+exports.secret = "secret 128 string";
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,8 +4,8 @@ const CryptoJS = require("crypto-js");
 const jwt = require('jsonwebtoken');
 const moment = require('moment');
 const _ =require('lodash');
+const { message, secret } = require('../config/const')
 const User = require('../models/user.model')
-const secret = 'secret 128 string';
 
 // 新建用户
 exports.create = function (req, res, next) {
@@ -14,21 +14,23 @@ exports.create = function (req, res, next) {
   .then(savedUser => {
     res.json(savedUser)
   })
-  .catch(e => next(e));
+  .catch(e => {
+    res.status(400).send(message.error400);
+  });
 };
 
 // 获取某个用户
 exports.get = function (req, res, next) {
   var id = req.params.id;
   if(!ObjectID.isValid(id)){
-    return res.status(404).send();
+    return res.status(404).send(message.error402);
   }
   User.findById(id)
   .then(user => {
     res.json(user)
   })
   .catch(e => {
-    next(e)
+    res.status(400).send(message.error400);
   });
 };
 
@@ -36,17 +38,17 @@ exports.get = function (req, res, next) {
 exports.update = function (req, res, next) {
   var id = req.params.id;
   if(!ObjectID.isValid(id)){
-    return res.status(404).send();
+    return res.status(404).send(message.error402);
   }
   User.findByIdAndUpdate(id,{$set:req.body},{new:false})
   .then((user)=>{
     if(!user){
-      return res.status(404).send();
+      return res.status(404).send(message.error404);
     }
     res.send({user});
   })
   .catch((e)=>{
-    res.status(400).send();
+    res.status(400).send(message.error400);
   });
 };
 
@@ -67,11 +69,11 @@ exports.remove = function (req, res, next) {
   }
   User.findByIdAndRemove(id, function(err, user) {
     if (err || user === null) {
-      return res.status(404).send({status:404, "msg":"未找到该用户或该用户已被删除","type":"warning"});
+      return res.status(404).send(message.error404);
     };
     res.json(200, user);
   }).catch(e=>{
-    res.status(400).send();
+    res.status(400).send(message.error400);
   });
 };
 
@@ -80,7 +82,7 @@ exports.refresh_token = function(req,res,next){
   var token = req.body.token;
   jwt.verify(token, secret, function(err, decoded) {
     if(err) {
-      res.status(401).send();
+      res.status(401).send(message.error401);
     }else{
       // 通过_id查询用户是否存在，找到用户并将userPwd剔除
       var id = decoded._doc._id;
@@ -91,7 +93,7 @@ exports.refresh_token = function(req,res,next){
         res.json({"msg":"yes","userName":decoded.userName,"token":token,"expires":expires});
       })
       .catch(e => {
-        next(e)
+        res.status(400).send(message.error400);
       });
 
     }
@@ -112,7 +114,7 @@ exports.login = function(req,res,next){
       var token =  jwt.sign(user, secret, { expiresIn: 60*1 })
       res.json({"msg":"登录成功","userName":user.userName,"token":token,"expires":expires});
     }else{
-      res.send(401, {status:401, message: '用户名密码错误，请重新登录', type:'error'});
+      res.status(401).send(message.error401);
     }
   });
 };
